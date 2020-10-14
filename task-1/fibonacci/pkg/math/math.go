be015388b693c957b05b84779cd9adf3eb149262
lokideos <archmage09@gmail.com>
2020-10-14 15:26:36 +0300

    Minor code style refactoring

--- task-1/fibonacci/pkg/math/math.go
+++ task-1/fibonacci/pkg/math/math.go
@@ -3,13 +3,13 @@ package math
 // Fibonacci function calculates last fibonacci number in fibonacci sequence given the provided 'max' variable via user input
 // The result have to be less than 'max' variable
 func Fibonacci(max int) int {
-	fibArr := []int{1, 1}
+	fibonacciSequence := []int{1, 1}
 
-	for nextElement(fibArr) < max {
-		fibArr = append(fibArr, nextElement(fibArr))
+	for nextElement(fibonacciSequence) < max {
+		fibonacciSequence = append(fibonacciSequence, nextElement(fibonacciSequence))
 	}
 
-	return fibArr[len(fibArr)-1]
+	return fibonacciSequence[len(fibonacciSequence)-1]
 }
 
 func nextElement(arr []int) int {
