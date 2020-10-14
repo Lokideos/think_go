feb851ae9d0b79c7bd801a6a817767c8664a8d56
lokideos <archmage09@gmail.com>
2020-10-14 23:01:40 +0300

    Add flag for calculation fibonacci nubmer according to user input

--- task-1-fibonacci/cmd/calculation/main.go
+++ task-1-fibonacci/cmd/calculation/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Lokideos/think_go/task-1-fibonacci/pkg/sequencer"
 )
 
+const (
+	// defaultFibnonacciMax is a default maximum nubmer during search for last number in Fibonacci sequence
+	defaultLastNumberIndex = 20
+)
+
 func main() {
-	lastNumberIndex := 20
+	var lastNumberIndex int
+	flag.IntVar(&lastNumberIndex, "f", defaultLastNumberIndex, "Number to compute in Fibonacci Sequence")
+	flag.Parse()
+
 	result := sequencer.Fibonacci(lastNumberIndex)
 	fmt.Println(result)
 }
